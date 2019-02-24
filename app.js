1d071bf17fefaacd98a32ba91f9c87897a6e470c
Sriram Manthrala <sriram_manthrala@ad.infosys.com>
2019-02-24 16:06:43 -0600

    initial import

---
+++ app.js
@@ -0,0 +1,14 @@
+"use strict";
+process.title = 'quote-example';
+var clientPort = process.env.PORT || 3100;
+var http = require('http');
+var clientExpress = require('express');
+const clientApp = clientExpress();
+var httpserver = http.Server(clientApp);
+
+clientApp.get('/',(req,res)=>{
+  res.sendFile(__dirname+"/public/server.html");
+});
+
+clientApp.use(clientExpress.static('public'));
+httpserver.listen(clientPort, () => console.log(`Example app listening on port ${clientPort}!`));
