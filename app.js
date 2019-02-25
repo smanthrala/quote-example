09a04913c5e5cd7ee7ed9455dbb6fac312453d09
Sriram Manthrala <sriram_manthrala@ad.infosys.com>
2019-02-24 21:03:29 -0600

    refactored

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const clientApp = clientExpress();
 var httpserver = http.Server(clientApp);
 
 clientApp.get('/',(req,res)=>{
-  res.sendFile(__dirname+"/public/server.html");
+  res.sendFile(__dirname+"server.html");
 });
 
-clientApp.use(clientExpress.static('public'));
+//clientApp.use(clientExpress.static('public'));
 httpserver.listen(clientPort, () => console.log(`Example app listening on port ${clientPort}!`));
